4e366a5fbd921b3f19bd1583979437aebe25cde1
Jingwen <zjwwjz@live.cn>
2017-03-02 13:14:17 -0500

    adjusted paddings

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
        data = $.csv.toArrays(result);
       //  console.log(data[0]);
       //console.log(data.length - 1);
-      drawShapes(data,10,40,20,true);
+      drawShapes(data,14,80,80,true);
     }
   });
 
@@ -28,7 +28,7 @@ $(document).ready(function() {
     var inputText = $("#inputText").val();
     console.log(inputWidth);
     $("svg").remove();
-    updateShapes(data,inputWidth,inputHeight,inputCurve,inputText);
+    //updateShapes(data,inputWidth,inputHeight,inputCurve,inputText);
   });
 
   $("#inputHeight").change(function() {
@@ -38,7 +38,7 @@ $(document).ready(function() {
     var inputText = $("#inputText").val();
     console.log(inputHeight);
     $("svg").remove();
-    updateShapes(inputWidth,inputHeight,inputCurve,inputText);
+    //updateShapes(inputWidth,inputHeight,inputCurve,inputText);
   });
 
   $("#inputCurve").change(function() {
@@ -48,7 +48,7 @@ $(document).ready(function() {
     var inputText = $("#inputText").val();
     console.log(inputCurve);
     $("svg").remove();
-    updateShapes(inputWidth,inputHeight,inputCurve,inputText);
+    //updateShapes(inputWidth,inputHeight,inputCurve,inputText);
   });
 
   // $("#inputText").change(function() {
@@ -64,17 +64,22 @@ $(document).ready(function() {
 
 function drawShapes(data,step,base,ratio,showText){
 
+  var h = (base+ratio) * Math.round(data.length / num);
+
 var r  = new Rune({
      container: "#canvas",
      width: (w - 30),
-     height: 2000,
+     height: 4000,
      debug: false,
    });
 
   for (var i = 1; i < (data.length - 1); i++) {
     var year = data[i][0];
-    var x = 20 + 150 * (i % 8);
-    var y = 100 + 100 * Math.round(i / 8);
+
+    var num = Math.round((w-100)/step/13);
+    var x = 20 + step*14 * (i % num);
+    var y = 100 + (base+ratio) * Math.round(i / num);
+
     // var step = 10; //input width
     // var ratio = 20; //input curve
     // var base = 30; //input height
@@ -97,45 +102,4 @@ var r  = new Rune({
 
   }
   r.draw();
-}
-
-function updateShapes(data,step,base,ratio,showText){
-//var context = 	$( "canvas" ).children().remove();
-  //context.clearRect(0, 0, canvas.width, canvas.height);
-// $( "#canvas" ).children().remove();
-// console.log($( "#canvas" ).children());
-
-var r  = new Rune({
-     container: "#canvas",
-     width: (w - 30),
-     height: 1000,
-     debug: false,
-   });
-
-    for (var i = 1; i < (data.length - 1); i++) {
-      var year = data[i][0];
-      var x = 20 + 150 * (i % 8);
-      var y = 100 + 100 * Math.round(i / 8);
-      // var step = 10; //input width
-      // var ratio = 20; //input curve
-      // var base = 30; //input height
-      var patternPath = r.path(x, y)
-        .stroke(0)
-        .fill(false)
-        .strokeWidth(2);
-
-      for (var j = 1; j < 12; j++) {
-        var temp = data[i][j];
-        //console.log(data[0][j]);
-        //console.log(temp);
-        //patternPath.lineTo((step * j),(data[i][j] * ratio+base))
-        patternPath.curveTo((step * j), (data[i][j] * ratio + base), (step * j + step * (j + 1)) / 2, ((data[i][j] * ratio + base) + (data[i][j + 1] * ratio + base)) / 2)
-
-      }
-      patternPath.curveTo((step * 12), (data[i][12] * ratio + base), (step * 12 + step * 13) / 2, ((data[i][12] * ratio + base) + 0) / 2)
-      patternPath.lineTo((step * 13), 0)
-      patternPath.closePath();
-
-    }
-      r.draw();
 }
